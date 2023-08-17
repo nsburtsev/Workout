4f4ba00010fa9d351cc8e69dd90a8d788ab2b8b3
nsburtsev <burtsev.ns@gmail.com>
2023-08-17 21:35:08 +0600

    Add progress view

--- Workout/Controllers/Session/SessionController.swift
+++ Workout/Controllers/Session/SessionController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class SessionController: BaseController {
-    private let timerView: WABaseInfoView = {
-        let view = WABaseInfoView(with: "Test", buttonTitle: "Test Button".uppercased())
+    private let timerView: TimerView = {
+        let view = TimerView()
         
         return view
     }()
@@ -29,7 +29,7 @@ extension SessionController {
             timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
             timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            timerView.heightAnchor.constraint(equalToConstant: 300)
+            timerView.heightAnchor.constraint(equalToConstant: 500)
         ])
     }
     
