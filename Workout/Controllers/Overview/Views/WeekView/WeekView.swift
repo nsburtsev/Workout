09ff7e41f1efe944da349e2b9f811dad286e666e
nsburtsev <burtsev.ns@gmail.com>
2023-08-18 20:06:38 +0600

    Add circle and dot layers

--- Workout/Controllers/Overview/Views/WeekView/WeekView.swift
+++ Workout/Controllers/Overview/Views/WeekView/WeekView.swift
@@ -9,9 +9,12 @@ import UIKit
 
 final class WeekView: BaseView {
     
-    private let calendar = Calendar.current
-    
-    private let stackView = UIStackView()
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 7
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
 }
 
 extension WeekView {
@@ -34,13 +37,10 @@ extension WeekView {
     
     override func configureAppearance() {
         super.configureAppearance()
-        
-        stackView.spacing = 7
-        stackView.distribution = .fillEqually
-        
+
         var weekdays = Date.calendar.shortStandaloneWeekdaySymbols
         
-        if Date.calendar.firstWeekday == 1 {
+        if Date.calendar.firstWeekday == 2 {
             let sun = weekdays.remove(at: 0)
             weekdays.append(sun)
         }
