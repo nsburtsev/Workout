bc0e022fb8761735c60b69cd88de2534f50ae20e
nsburtsev <burtsev.ns@gmail.com>
2023-08-28 23:56:21 +0600

    Add DailyPerformanceView

--- Workout/Controllers/Progress/ProgressController.swift
+++ Workout/Controllers/Progress/ProgressController.swift
@@ -8,13 +8,50 @@
 import UIKit
 
 class ProgressController: WABaseController {
+    
+    private let dailyPerformanceView = DailyPerformanceView(with: R.Strings.Progress.dailyPerformance, buttonTitle: R.Strings.Progress.last7Days)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension ProgressController {
+    override func setupViews() {
+        super.setupViews()
         
-        title = "Workout Progress"
+        view.setupView(dailyPerformanceView)
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
         
-        addNavBarButton(at: .left, with: "Export")
-        addNavBarButton(at: .right, with: "Details")
+        NSLayoutConstraint.activate([
+            dailyPerformanceView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            dailyPerformanceView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            dailyPerformanceView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            dailyPerformanceView.heightAnchor.constraint(equalTo: dailyPerformanceView.widthAnchor, multiplier: 0.68),
+            
+
+        ])
+        
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+        
+        title = R.Strings.NavBar.progress
+        navigationController?.tabBarItem.title = R.Strings.TabBar.title(for: .progress)
+        
+        addNavBarButton(at: .left, with: R.Strings.Progress.navBarLeft)
+        addNavBarButton(at: .right, with: R.Strings.Progress.navBarRight)
+        
+        dailyPerformanceView.configure(with: [.init(value: "1", heightMultiplier: 0.2, title: "Mon"),
+                                              .init(value: "2", heightMultiplier: 0.4, title: "Tue"),
+                                              .init(value: "3", heightMultiplier: 0.6, title: "Wed"),
+                                              .init(value: "4", heightMultiplier: 0.8, title: "Thu"),
+                                              .init(value: "5", heightMultiplier: 1, title: "Fri"),
+                                              .init(value: "3", heightMultiplier: 0.6, title: "Sat"),
+                                              .init(value: "2", heightMultiplier: 0.4, title: "Sun"),])
     }
 }
